8cbea990ddad0b98b5b7c1c487649e4de0279f1c
Timur Stegmann <github.timur@4stegmanns.de>
2021-05-28 18:54:08 +0200

    Updated to new Feature

---
+++ README.md
@@ -0,0 +1,124 @@
+[![Version](https://img.shields.io/badge/Symcon-PHP--Modul-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/)
+[![Version](https://img.shields.io/badge/Symcon%20Version-5.0%20%3E-blue.svg)](https://www.symcon.de/produkt/)
+# MHC - Magic Home Controller `copied`
+
+Dieses Modul ist ***kopiert*** von @Wilkware, weil es um die Funktion `SetCustomPattern()` erweitert wurde. IP-Symcon Modul für die Ansteuerung von WiFi LED Controller der Firma _Magic Home_.
+
+## Inhaltverzeichnis
+
+1. [Funktionsumfang](#1-funktionsumfang)
+2. [Voraussetzungen](#2-voraussetzungen)
+3. [Installation](#3-installation)
+4. [Einrichten der Instanzen in IP-Symcon](#4-einrichten-der-instanzen-in-ip-symcon)
+5. [Statusvariablen und Profile](#5-statusvariablen-und-profile)
+6. [WebFront](#6-webfront)
+7. [PHP-Befehlsreferenz](#7-php-befehlsreferenz)
+8. [Versionshistorie](#8-versionshistorie)
+
+### 1. Funktionsumfang
+
+Das Modul dient zur Ansteuerung von LED Stripes mittels eines WiFi LED Controllers des Herstellers Magic Home.
+Das sind Controller vom Typ LD382 bzw. LD382A.
+
+Wenn jemand noch weitere kennt, bitte einfach bei mir melden!
+
+### 2. Voraussetzungen
+
+* ab IP-Symcon Version 5.0
+
+### 3. Installation
+
+* Über den Modul Store das Modul 'Magic Home Controller' installieren.
+* Alternativ Über das Modul-Control folgende URL hinzufügen.  
+`https://github.com/Wilkware/IPSymconMHC` oder `git://github.com/Wilkware/IPSymconMHC.git`
+
+### 4. Einrichten der Instanzen in IP-Symcon
+
+* Unter "Instanz hinzufügen" ist das 'Magic Home Controller'-Modul (Alias: LED Controller) unter dem Hersteller '(Sonstige)' aufgeführt.
+
+__Konfigurationsseite__:
+
+Die Konfiguration beinhaltet die Zuweisung der IP-Adresse und der Auswahl der Farbkanal-Reihenfolge (RGB-Pins).  
+Derzeit werden Stripes mit der Reihenfolge GRB (Gelb, Rot, Blau) und BRG (Blau, Rot, Gelb) unterstützt.  
+Die Reihenfolge der Pins beginnt immer ausgehend vom GND-Pin (12V Pin).
+
+Darüber hinaus kann noch ausgewählt werden, ob die Debug-Meldungen auch im Status Logging (Log Meldungen) ausgegeben werden sollen.
+
+Name               | Beschreibung
+------------------ | ---------------------------------
+WiFi Controller IP | IP-Adresse des Controlers im lokalen WLAN (zb. 192.168.0.10)
+RGB Pin Belegung   | Reihenfolge der Farb-Pins (GRB oder BRG), Standard ist GRB
+
+### 5. Statusvariablen und Profile
+
+Ident         | Name                | Typ       |  Profil                      | Beschreibung
+------------- | ------------------- | --------- | ---------------------------- | -------------------------------------------------------
+Brightness    | Helligkeit          | Integer   | ~Intensity.100               | Helligkeitswert von 0 bis 100%
+Color         | Farbe               | Integer   | ~HexColor                    | Farbwert
+Mode          | Modus               | Integer   | MHC.ModeGRB oder MHC.ModeBRG | Manueller Farbmodus oder vordefinierter Funktionsmodus
+Power         | Aktiv               | Boolean   | ~Switch                      | An/Aus Schalter
+Speed         | Geschwindigkeit     | Integer   | ~Intensity.100               | Geschwindigkeitswert von 0 bis 100%
+
+### 6. WebFront
+
+Man kann die Statusvariaben direkt im WebFront verlinken.
+
+### 7. PHP-Befehlsreferenz
+
+`void MHC_SetBrightness(int $InstanzID, int $Brightness);`  
+Setzt die Helligkeit auf $Brightness. Die Funktion liefert keinerlei Rückgabewert.
+
+`void MHC_SetColor(int $InstanzID, int $Color);`  
+Setzt den Farbwert auf $Color. Die Funktion liefert keinerlei Rückgabewert.
+
+`void MHC_SetCustomPatternColor(int $InstanzID, int $Color);`  
+Setzt den Farbwert auf $Color. Die Funktion liefert keinerlei Rückgabewert.
+
+`void MHC_SetMode(int $InstanzID, int $Mode);`  
+Setzt den Anzeigemodus auf auf $Mode. Die Funktion liefert keinerlei Rückgabewert.
+
+`void MHC_SetCustomPattern(int $InstanzID, int $Speed, string $Transition_Type, array $color_list );`  
+Setzt den gewünschten Modus, Geschwindigkeit und Farbkombination. Diese Funktion wird nicht in den Variablen
+aufgeführt, dass heißt das sei nur im Code ausgelöst werden kann:
+```php
+// Transition: fade // jump // strobe
+// Colors : [ [r,g,b], [r.g.b], ... ]
+MHC_SetCustomPattern( 150, 'fade', [ [255,0,0], [0,255,0], [0,0,255] ]);
+```
+
+### 8. Versionshistorie
+
+v1.2.20190812
+
+* _NEU_: Anpassungen für Module Store
+* _NEU_: Vereinheitlichungen, Umstellung auf Libs
+* _FIX_: Fehler in Profilen korrigiert
+
+v1.1.20190225
+
+* _NEU_: Umbenennungen, Vereinheitlichungen, StyleCI uvm.
+
+v1.0.20180415
+
+* _NEU_: Initialversion
+
+### Danksagung
+
+Dieses Modul basiert auf dem Modul von ...
+
+* _Spoosie_ : Modul _KH\_LEDWiFiController_ <https://github.com/Spoosie/KH_LEDWiFiController>
+
+Vielen Dank für die hervorragende und tolle Arbeit!
+
+### Entwickler
+
+* Heiko Wilknitz ([@wilkware](https://github.com/wilkware))
+
+### Spenden
+
+Die Software ist für die nicht kommzerielle Nutzung kostenlos, Schenkungen als Unterstützung für den Entwickler bitte hier:  
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8816166" target="_blank"><img src="https://www.paypalobjects.com/de_DE/DE/i/btn/btn_donate_LG.gif" border="0" /></a>
+
+### Lizenz
+
+[![Licence](https://licensebuttons.net/i/l/by-nc-sa/transparent/00/00/00/88x31-e.png)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
---
+++ Wifi_LED_Controler/module.php
@@ -0,0 +1,339 @@
+<?php
+
+require_once __DIR__.'/../libs/traits.php';  // Allgemeine Funktionen
+
+class WifiLEDControler extends IPSModule
+{
+    use ProfileHelper, DebugHelper;
+    // Modul
+    private $moduleName = 'Magic Home Control';
+    // GRB Profil array
+    private $assoGRB = [
+      [0, 'Manuell', '', 0x000000],
+      [37, '7-stufiger Farbdurchlauf', '', 0x000000],
+      [38, 'Rot pulsierend', '', 0xFF0000],
+      [39, 'Grün pulsierend', '', 0x00FF00],
+      [40, 'Blau pulsierend', '', 0x0000FF],
+      [41, 'Gelb pulsierend', '', 0xFFFF00],
+      [42, 'Türkis pulsierend', '', 0x00FFFF],
+      [43, 'Violett pulsierend', '', 0xFF00FF],
+      [44, 'Weiss pulsierend', '', 0xFFFFFF],
+      [45, 'Rot Grün pulsierend', '', 0xF0F000],
+      [46, 'Rot Blau pulsierend', '', 0xF000F0],
+      [47, 'Grün Blau pulsierend', '', 0x00F0F0],
+      [48, '7-stufig blitzend', '', 0xA0A0A0],
+      [49, 'Rot blitzend', '', 0xFF0000],
+      [50, 'Grün blitzend', '', 0x00FF00],
+      [51, 'Blau blitzend', '', 0x0000FF],
+      [52, 'Gelb blitzend', '', 0xFFFF00],
+      [53, 'Türkis blitzend', '', 0x00FFFF],
+      [54, 'Violett blitzend', '', 0xFF00FF],
+      [55, 'Weiss blitzend', '', 0xFFFFFF],
+    ];
+    // BRG Profil array
+    private $assoBRG = [
+      [0, 'Manuell', '', 0x000000],
+      [37, '7-stufiger Farbdurchlauf', '', 0x000000],
+      [38, 'Rot pulsierend', '', 0xFF0000],
+      [39, 'Blau pulsierend', '', 0x0000FF],
+      [40, 'Grün pulsierend', '', 0x00FF00],
+      [41, 'Violett pulsierend', '', 0xFF00FF],
+      [42, 'Türkis pulsierend', '', 0x00FFFF],
+      [43, 'Gelb pulsierend', '', 0xFFFF00],
+      [44, 'Weiss pulsierend', '', 0xFFFFFF],
+      [45, 'Rot Blau pulsierend', '', 0xF000F0],
+      [46, 'Rot Grün pulsierend', '', 0xF0F000],
+      [47, 'Grün Blau pulsierend', '', 0x00F0F0],
+      [48, '7-stufig blitzend', '', 0xA0A0A0],
+      [49, 'Rot blitzend', '', 0xFF0000],
+      [50, 'Blau blitzend', '', 0x0000FF],
+      [51, 'Grün blitzend', '', 0x00FF00],
+      [52, 'Violett blitzend', '', 0xFF00FF],
+      [53, 'Türkis blitzend', '', 0x00FFFF],
+      [54, 'Gelb blitzend', '', 0xFFFF00],
+      [55, 'Weiss blitzend', '', 0xFFFFFF],
+    ];
+
+    /**
+     * Create.
+     */
+    public function Create()
+    {
+        // Never delete this line!
+        parent::Create();
+        // Config Variablen
+        $this->RegisterPropertyString('TCPIP', '127.0.0.1');
+        $this->RegisterPropertyString('RGB', '012');
+
+        // Variablen Profile einrichten
+        $this->RegisterProfile(vtInteger, 'MHC.ModeGRB', 'Bulb', '', '', 0, 0, 0, 0, $this->assoGRB);
+        $this->RegisterProfile(vtInteger, 'MHC.ModeBRG', 'Bulb', '', '', 0, 0, 0, 0, $this->assoBRG);
+
+        // Variablen erzeugen
+        $varID = $this->RegisterVariableBoolean('Power', 'Aktiv', '~Switch', 0);
+        $this->EnableAction('Power');
+        $varID = $this->RegisterVariableInteger('Color', 'Farbe', '~HexColor', 1);
+        $this->EnableAction('Color');
+        $varID = $this->RegisterVariableInteger('Speed', 'Geschwindigkeit', '~Intensity.100', 2);
+        $this->EnableAction('Speed');
+        $varID = $this->RegisterVariableInteger('Brightness', 'Helligkeit', '~Intensity.100', 3);
+        $this->EnableAction('Brightness');
+        $varID = $this->RegisterVariableInteger('Mode', 'Modus', 'MHC.ModeGRB', 4);
+        $this->EnableAction('Mode');
+    }
+
+    /**
+     * Destroy.
+     */
+    public function Destroy()
+    {
+        // Never delete this line!
+        parent::Destroy();
+    }
+
+    /**
+     * Apply Configuration Changes.
+     */
+    public function ApplyChanges()
+    {
+        // Never delete this line!
+        parent::ApplyChanges();
+        // Debug message
+        $tcpIP = $this->ReadPropertyString('TCPIP');
+        $rgbID = $this->ReadPropertyString('RGB');
+        // IP Check
+        if (filter_var($this->ReadPropertyString('TCPIP'), FILTER_VALIDATE_IP) !== false) {
+            $this->SetStatus(102);
+        } else {
+            $this->SetStatus(201);
+        }
+        // Setup variable profil
+        if ($rgbID == '012') {
+            $this->RegisterVariableInteger('Mode', 'Modus', 'MHC.ModeGRB', 4);
+        } else {
+            $this->RegisterVariableInteger('Mode', 'Modus', 'MHC.ModeBRG', 4);
+        }
+        $this->SendDebug('ApplyChanges', 'IP='.$tcpIP.', RGB='.$rgbID, 0);
+    }
+
+    /**
+     * Call by visual changes.
+     */
+    public function RequestAction($ident, $value)
+    {
+        // Debug
+        $this->SendDebug('RequestAction', 'RequestAction: ($ident,$value)', 0);
+
+        switch ($ident) {
+            // Switch Power On/Off
+            case 'Power':
+                $on = [0x71, 0x23, 0x0f];
+                $off = [0x71, 0x24, 0x0f];
+                if ($value) {
+                    $this->SendData($on);
+                } else {
+                    $this->SendData($off);
+                }
+                SetValue($this->GetIDForIdent($ident), $value);
+                break;
+            // Set Speed value
+            case 'Speed':
+                SetValue($this->GetIDForIdent($ident), $value);
+                $this->SendFunction();
+                break;
+            // Set Display Mode
+            case 'Mode':
+                IPS_SetDisabled($this->GetIDForIdent('Speed'), !$value);
+                IPS_SetDisabled($this->GetIDForIdent('Color'), $value);
+                IPS_SetDisabled($this->GetIDForIdent('Brightness'), $value);
+                SetValue($this->GetIDForIdent($ident), $value);
+                // Manual mode.
+                if ($value == 0) {
+                    $this->SendColor();
+                }
+                // Functional mode
+                else {
+                    $this->SendFunction();
+                }
+                break;
+            case 'Color':
+            case 'Brightness':
+                SetValue($this->GetIDForIdent($ident), $value);
+                $this->SendColor();
+                break;
+            default:
+                throw new Exception('Invalid Ident');
+        }
+    }
+
+    /**
+     * This function will be available automatically after the module is imported with the module control.
+     * Using the custom prefix this function will be callable from PHP and JSON-RPC through:.
+     *
+     * MHC_SetBrightness(int $InstanzID, int $Brightness);
+     */
+    public function SetBrightness(int $brightness)
+    {
+        $this->RequestAction('Brightness', $brightness);
+    }
+
+    /**
+     * This function will be available automatically after the module is imported with the module control.
+     * Using the custom prefix this function will be callable from PHP and JSON-RPC through:.
+     *
+     * MHC_SetColor(int $InstanzID, int $Color);
+     */
+    public function SetColor(int $color)
+    {
+        $this->RequestAction('Color', $color);
+    }
+
+    /**
+     * Send color data. This part is by @parcivad
+     */
+    function SetCustomPattern( int $speed, string $transition_type, array $color_list ) {
+        // Call for custom pattern
+        $data = [ 0x51 ];
+
+        // COLORS - - - Add colors to the $data [max. of 16 colors, the empty slots must be filled by 1, 2, 3. Between each color (rgb) must be a 0 to split]
+        for($i = 0; $i < 16; $i++) {
+            if ( isset( $color_list[$i] ) ) {
+                array_push( $data, $color_list[$i][0], $color_list[$i][1], $color_list[$i][2], 0);
+            } else {
+                array_push( $data, 1, 2, 3, 0 );
+            }
+        }
+        // SPEED - - - Add Speed to the $data list
+        $speed = min(100, max(0, $speed));
+        array_push( $data, (30 - (($speed / 100) * 30)) + 1 );
+        // TRANSITION - - - Add a Transition type to the $data list
+        switch ( $transition_type ) {
+            case "fade":
+                array_push( $data, 0x3a);
+                break;
+            case "jump":
+                array_push( $data, 0x3b);
+                break;
+            case "strobe":
+                array_push( $data, 0x3c);
+                break;
+        }
+        // BRIGHTNESS - - - Add Brightness to the whole effect, this option can't be set or changed!
+        array_push( $data, 0xff, 0x0f );
+        SendData( $data );
+    }
+
+    /**
+     * This function will be available automatically after the module is imported with the module control.
+     * Using the custom prefix this function will be callable from PHP and JSON-RPC through:.
+     *
+     * MHC_SetMode(int $InstanzID, int $Mode);
+     */
+    public function SetMode(int $mode)
+    {
+        $this->RequestAction('Mode', $mode);
+    }
+
+    /**
+     * This function will be available automatically after the module is imported with the module control.
+     * Using the custom prefix this function will be callable from PHP and JSON-RPC through:.
+     *
+     * MHC_SetPower(int $InstanzID, bool $Power);
+     */
+    public function SetPower(bool $power)
+    {
+        $this->RequestAction('Power', $power);
+    }
+
+    /**
+     * Send function data.
+     */
+    private function SendFunction()
+    {
+        $data = [0x61, 0x00, 0x00, 0x0F];
+        $mode = GetValue($this->GetIDForIdent('Mode'));
+        $speed = 100 - GetValue($this->GetIDForIdent('Speed'));
+        if ($speed > 100) {
+            $speed = 100;
+        } elseif ($speed < 1) {
+            $speed = 1;
+        }
+        $data[1] = $mode;
+        $data[2] = $speed;
+        $this->SendData($data);
+    }
+
+    /**
+     * Send color data.
+     */
+    private function SendColor()
+    {
+        $data = [0x31, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0F];
+        $brightness = GetValue($this->GetIDForIdent('Brightness')) / 100;
+        $color = GetValue($this->GetIDForIdent('Color'));
+        $rgb = [0x00, 0x00, 0x00];
+        $rgb[0] = (($color >> 16) & 0xFF); // red
+        $rgb[1] = (($color >> 8) & 0xFF); // green
+        $rgb[2] = ($color & 0xFF); // blue
+        // map with brightness
+        $rgb[0] *= $brightness;
+        $rgb[1] *= $brightness;
+        $rgb[2] *= $brightness;
+        // map rgb channel
+        $channel = $this->ReadPropertyString('RGB');
+        $index = (int) $channel[0];
+        $this->SendDebug('SendColor', "0 -> $index", 0);
+        $data[1] = floor($rgb[$index]);
+        $index = (int) $channel[1];
+        $this->SendDebug('SendColor', "1 -> $index", 0);
+        $data[2] = floor($rgb[$index]);
+        $index = (int) $channel[2];
+        $this->SendDebug('SendColor', "2 -> $index", 0);
+        $data[3] = floor($rgb[$index]);
+        // send data
+        $this->SendData($data);
+    }
+
+    /**
+     * Send data array to controller.
+     */
+    private function SendData($values)
+    {
+        $path = 'tcp://'.$this->ReadPropertyString('TCPIP');
+        $socket = @fsockopen($path, 5577, $errno, $errstr, 5);
+        // Check Socket
+        if (!$socket) {
+            $this->SendDebug('SendData', $path." -> $errstr ($errno)", 0);
+
+            return;
+        } else {
+            $this->SendDebug('SendData', "Verbindung aufgebaut '".$path."'", 0);
+        }
+        $send = '';
+        foreach ($values as $value) {
+            $send .= chr($value);
+            $data[] = $value;
+        }
+        $check = $this->GetChecksum($values);
+        $send .= chr($check);
+        $data[] = $check;
+        // send data
+        fwrite($socket, $send);
+        $this->SendDebug('SendData', 'Sende Daten='.implode(',', $data), 0);
+        // close socket
+        fclose($socket);
+    }
+
+    /**
+     * Calculatue checksum for given data.
+     */
+    private function GetChecksum($values)
+    {
+        $checksum = array_sum($values);
+        $checksum = dechex($checksum);
+        $checksum = substr($checksum, -2);
+        $checksum = hexdec($checksum);
+
+        return $checksum;
+    }
+}
